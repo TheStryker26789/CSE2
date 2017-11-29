368a8c3d919f19882accd14913102acf388a8be2
Mr. Wiebe <kyle.wiebe@fresnounified.org>
2017-11-29 08:49:17 -0800

    Initial commit

---
+++ Basic Python Notes.py
@@ -0,0 +1,20 @@
+print("Hello World!")
+# print("Welcome to class!")
+
+# This is a comment
+# This does not run, but allows me to
+# write notes to myself (or a teacher)
+
+"""
+This is a multi-line comment
+"""
+
+# Variables
+a = -5
+b = -4
+c = "hello!"
+d = "Jeffery"
+
+# print(-5 - (-1))
+# print(a - b)
+print(c)
