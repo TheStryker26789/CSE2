0dd4a8e7f48c8ace1140c2a044148d6e05691eb2
Mr. Wiebe <kyle.wiebe@fresnounified.org>
2017-12-08 08:51:14 -0800

    12.8.17

--- Warmups.py
+++ Warmups.py
@@ -23,3 +23,50 @@ add_py("John") == "John.py"
 def add_py(name):
     print("%s.py" % name)  # Solution 1
     print(name + ".py")  # Solution 2
+
+
+# 12.6.17
+"""Write a function called "add"
+which takes three parameters 
+and prints the sum of the numbers
+"""
+
+
+def add(num1, num2, num3):
+    print(num1 + num2 + num3)
+
+add(15, 18, 9000)
+add(80, 90, 100)
+
+
+# 12.7.17
+# Write a function called "repeat"
+# that takes one parameter (string)
+# and prints it three times.
+#
+# example:
+# repeat("Hello") prints:
+# Hello
+# Hello
+# Hello
+
+def repeat(sentence):
+    print(sentence)
+    print(sentence)
+    print(sentence)
+
+    print(sentence + "/n" + sentence + "/n" + sentence + "/n")
+
+    for x in range(3):
+        print(sentence)
+
+# 12.8.17
+"""
+Write a function called "date"
+that takes in three parameters,
+"month", "day", and "year" and 
+prints out the date, separated by a "/"
+
+example:
+date(12, 8, 17) == "12/8/17"
+"""
